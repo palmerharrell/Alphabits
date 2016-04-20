f54159f7c648a344d3297583e350f335e465ecd0
Palmer Harrell <palmerharrell@me.com>
2016-04-19 23:10:21 -0500

    First commit: Set up VS project and test suite. Created class and empty methods.

---
+++ AlphaBitsTests/UnitTest1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Alphabits;
+
+namespace AlphaBitsTests
+{
+  [TestClass]
+  public class UnitTest1
+  {
+    AlphaList testObj = new AlphaList();
+    [TestMethod]
+    public void listLengthTest()
+    {
+      Assert.AreEqual(0, testObj.listLength());
+    }
+  }
+}
---
+++ Alphabits/AlphaList.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alphabits
+{
+  public class AlphaList
+  {
+    // public AlphaList()
+    // {
+    //   // Anything to put here?
+    // }
+
+    public List<char> letterList;
+
+    public void addChar(char newChar)
+    {
+      Console.WriteLine(newChar);
+    }
+
+    public int listLength()
+    {
+      return letterList.Count;
+    }
+  }
+}
